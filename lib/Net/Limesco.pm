15c8d55d4f60db9a17973d25332885fe967c28ca
Sjors Gielen <sjors@sjorsgielen.nl>
2012-10-27 21:15:43 +0200

    Add first version of Net::Limesco

---
+++ lib/Net/Limesco.pm
@@ -0,0 +1,50 @@
+package Net::Limesco;
+
+use 5.012004;
+use strict;
+use warnings;
+
+our $VERSION = "1.0";
+
+=head1 NAME
+
+Net::Limesco - Perl interface to the Limesco API
+
+=head1 SYNOPSIS
+
+  use Net::Limesco;
+
+=head1 DESCRIPTION
+
+This module is an interface to the Limesco API found at
+http://api.limesco.org/.
+
+=head1 SEE ALSO
+
+=over
+
+=item *
+
+http://limesco.org/
+
+=item *
+
+http://wiki.limesco.org/
+
+=back
+
+=head1 AUTHOR
+
+Sjors Gielen, E<lt>sjors@limesco.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Sjors Gielen
+
+See the LICENSE file for the license on this module.
+
+=cut
+
+1;
+
+__END__
